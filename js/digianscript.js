7add7f21ed03cf6deccfde05425906c0858c6122
Art-Joga <00001093880284sp@al.educacao.sp.gov.br>
2025-05-29 15:54:19 -0300

    Add files via upload

---
+++ js/digianscript.js
@@ -0,0 +1,24 @@
+function irparaSobre() {
+    const imagem = document.getElementById('isobre');
+    const posicaoY = imagem.getBoundingClientRect().top;
+    window.scrollTo( {
+        top: posicaoY,
+        behavior: 'smooth'
+    });
+}
+function irparaServ() {
+    const imagem = document.getElementById('iserv');
+    const posicaoY = imagem.getBoundingClientRect().top;
+    window.scrollTo( {
+        top: posicaoY,
+        behavior: 'smooth'
+    });
+}
+function irparaContato() {
+    const imagem = document.getElementById('icontato');
+    const posicaoY = imagem.getBoundingClientRect().top;
+    window.scrollTo( {
+        top: posicaoY,
+        behavior: 'smooth'
+    });
+}
